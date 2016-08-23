4078f038fde61b656bf3b11615294bd7a3733976
Adrian Tello <adriantl7@gmail.com>
2016-08-23 23:49:15 +0200

    Create the MapFacade and refactor

--- js/components/Map.js
+++ js/components/Map.js
@@ -1,10 +1,8 @@
-import BABYLON from 'babylonjs';
+import MapFacade from '../map/MapFacade';
 import React from 'react';
 import Relay from 'react-relay';
 
 import ChangeTileTypeMutation from '../mutations/ChangeTileTypeMutation';
-
-const VIEWPORT_SIZE = 5;
 
 class Map extends React.Component {
     constructor(){
@@ -17,18 +15,12 @@ class Map extends React.Component {
             }
         };
 
-        this._materials = [];
-        this._tiles = [];
-
-        this._firstTileMesh = null;
-        this._firstTileBorderMesh = null;
-
-        this._scene = null;
+        this._mapFacade = null;
     }
     render() {
         const component = this;
 
-        if(this._scene !== null){
+        if(this._mapFacade !== null){
             this._update();
         }
 
@@ -37,67 +29,39 @@ class Map extends React.Component {
         );
     }
     _update(){
-        this._updateTileTypes();
+        this._mapFacade.setTileTypes(this.props.tileset.availableTileTypes);
         this._updateTiles();
-    }
-    _updateTileTypes(){
-        const oldAvailableTileTypes = this._oldProps.tileset.availableTileTypes;
-        const availableTileTypes = this.props.tileset.availableTileTypes;
-
-        if(oldAvailableTileTypes > availableTileTypes){
-            //There are fewer tile types
-            console.warn('Not implemented yet');
-        }else if(oldAvailableTileTypes < availableTileTypes){
-            //There are new tile types
-            for(let tileType = oldAvailableTileTypes; tileType < availableTileTypes; tileType++){
-                this._createTileTypeMaterial();
-            }
-        }
     }
     _updateTiles(){
         const component = this;
 
-        this._updateRowsNumber();
+        this._updateSize();
 
         const rows = this.props.tileset.rows;
         rows.forEach(function(currRow, rowIdx){
             component._updateRow.apply(component, [currRow, rowIdx]);
         });
     }
+    _updateSize(){
+        const rows = this.props.tileset.rows;
+
+        const numRows = rows.length;
+        const numCols = (numRows === 0)?0:rows[0].tiles.length;
+
+        this._mapFacade._setSize(numCols, numRows);
+    }
     _updateRow(row, rowIdx){
         const component = this;
 
-        const tileMeshes = this._tiles;
+        const mapFacade = this._mapFacade;
         const tiles = row.tiles;
 
-        //TODO tile number changed...
-
         tiles.forEach(function(tile, tileIdx){
-            const tileMesh = tileMeshes[rowIdx][tileIdx];
-
-            tileMesh.material = component._materials[tile.type];
+            mapFacade.setTileType(tileIdx, rowIdx, tile.type);
         });
     }
-    _updateRowsNumber(){
-        const rows = this.props.tileset.rows;
-
-        const oldRowsCount = this._oldProps.tileset.rows.length;
-        const rowsCount = rows.length;
-
-        //Check if the row number changed
-        if(oldRowsCount > rowsCount){
-            //There are fewer rows
-            console.warn('Not implemented yet');
-        }else if(oldRowsCount < rowsCount){
-            //There are new tile types
-            for(let rowNum = oldRowsCount; rowNum < rowsCount; rowNum++){
-                const row = rows[rowNum];
-                this._createTilesRow(rowNum, row);
-            }
-        }
-    }
-    _onTileTypeChanged(tileCoords){
-        const tile = this.props.tileset.rows[tileCoords.y].tiles[tileCoords.x];
+    _handleTileClicked(numCol, numRow){
+        const tile = this.props.tileset.rows[numRow].tiles[numCol];
         const newTileType = (tile.type + 1) % this.props.tileset.availableTileTypes;
 
         this.props.relay.commitUpdate(
@@ -110,182 +74,24 @@ class Map extends React.Component {
     componentDidMount(){
         const mapRef = this.refs.mapRef;
 
-        this._initBabylon(mapRef);
+        this._initMap(mapRef);
         this._update();
     }
-    _initBabylon(canvas){
-        const component = this;
-
-        // Load the BABYLON 3D engine
-        const engine = new BABYLON.Engine(canvas, true);
-        this._engine = engine;
-
-        const scene = this._createScene(canvas);
-        scene.ambientColor = new BABYLON.Color3(1, 1, 1);
-        scene.lightsEnabled = false;
-        scene.debugLayer.show();
-        this._scene = scene;
-
-        this._updateAspectRatio();
-
-        // Register a render loop to repeatedly render the scene
-        engine.runRenderLoop(function () {
-            scene.render();
-        });
+    _initMap(canvas){
+        const mapFacade = new MapFacade(canvas);
+        mapFacade.start();
+        this._mapFacade = mapFacade;
 
         // Watch for browser/canvas resize events
         window.addEventListener('resize', function () {
-            component._updateAspectRatio.apply(component);
-
-            component._engine.resize();
+            mapFacade.resize();
         });
-    }
-    _updateAspectRatio(){
-        const scene = this._scene;
 
-        const camera = this._mainCamera;
-
-        let height = this.refs.mapRef.height;
-        const width = this.refs.mapRef.width;
-
-        if (height == 0) height = 1;
-
-        /* Calculate aspect ratio --------------------------------------------------------------------*/
-        const aspectRatio = width / height;
-
-        if (width >= height)
-        {
-            camera.orthoLeft = -VIEWPORT_SIZE*aspectRatio;
-            camera.orthoRight = VIEWPORT_SIZE*aspectRatio;
-            camera.orthoBottom = -VIEWPORT_SIZE;
-            camera.orthoTop = VIEWPORT_SIZE;
-        }
-        else
-        {
-            camera.orthoLeft = -VIEWPORT_SIZE;
-            camera.orthoRight = VIEWPORT_SIZE;
-            camera.orthoBottom = -VIEWPORT_SIZE*aspectRatio;
-            camera.orthoTop = VIEWPORT_SIZE*aspectRatio;
-        }
+        this._mapFacade.setTileClickedListener(this._handleTileClicked.bind(this));
     }
     componentWillUnmount(){
         const engine = this._engine;
         engine.dispose();
-    }
-    _createScene(canvas){
-        const engine = this._engine;
-        const scene =  new BABYLON.Scene(engine);
-        scene.shadowsEnabled = false;
-
-        const camera = new BABYLON.TargetCamera('mainCamera', new BABYLON.Vector3(0, 0, -10), scene);
-       camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
-
-        // attach the camera to the canvas
-        camera.attachControl(canvas, false);
-
-        this._mainCamera = camera;
-
-        return scene;
-    }
-    _createTilesRow(tileY, row){
-        const scene = this._scene;
-        const tiles = row.tiles;
-
-        const newRow = [];
-
-        for (var tileX = 0; tileX < tiles.length; tileX++){
-            const tile = tiles[tileX];
-
-            const tileTag = this._createTileTag(tileX, tileY);
-
-            const tileMesh = this._createTileMesh(tileTag);
-            const tileBorderMesh = this._createTileBorderMesh(tileTag);
-            tileBorderMesh.parent = tileMesh;
-
-            const origMaterial = this._materials[tile.type];
-            tileMesh.material = origMaterial;
-            tileMesh.position.x = tileX;
-            tileMesh.position.y = tileY;
-
-            this._setUpTileActions(
-                tileMesh,
-                origMaterial,
-                {
-                    x: tileX,
-                    y: tileY
-                }
-            );
-
-            newRow.push(tileMesh);
-        }
-
-        this._tiles.push(newRow);
-    }
-    _createTileTag(tileX, tileY){
-        return 'tile-' + tileX + '-' + tileY;
-    }
-    _createTileMesh(tileTag){
-        const scene = this._scene;
-        let tileMesh = null;
-
-        if(this._firstTileMesh === null){
-            this._firstTileMesh = BABYLON.Mesh.CreatePlane(tileTag, 1.0, scene);
-            tileMesh = this._firstTileMesh;
-        }else{
-            tileMesh = this._firstTileMesh.clone(tileTag);
-        }
-        return tileMesh;
-    }
-    _createTileBorderMesh(tileTag){
-        const scene = this._scene;
-
-        let tileBorderMesh = null;
-
-        const borderMeshTag = 'border-' + tileTag;
-
-        if(this._firstTileBorderMesh === null){
-            this._firstTileBorderMesh = BABYLON.Mesh.CreateLines(borderMeshTag, [
-                new BABYLON.Vector3(-0.5, -0.5, 0),
-                new BABYLON.Vector3(0.5, -0.5, 0),
-                new BABYLON.Vector3(0.5, 0.5, 0),
-                new BABYLON.Vector3(-0.5, 0.5, 0),
-                new BABYLON.Vector3(-0.5, -0.5, 0)
-            ], scene);
-
-            tileBorderMesh = this._firstTileBorderMesh;
-        }else{
-            tileBorderMesh = this._firstTileBorderMesh.clone(borderMeshTag);
-        }
-        return tileBorderMesh;
-    }
-    _setUpTileActions(tileMesh, origMaterial, tileCoords){
-        const scene = this._scene;
-
-        tileMesh.actionManager = new BABYLON.ActionManager(scene);
-
-        this._setUpTileLeftPickAction(tileMesh, tileCoords);
-    }
-    _setUpTileLeftPickAction(tileMesh, tileCoords){
-        const component = this;
-        const scene = this._scene;
-
-        const tileLeftPickAction = new BABYLON.ExecuteCodeAction(
-            BABYLON.ActionManager.OnLeftPickTrigger,
-            function () {
-                component._onTileTypeChanged(tileCoords);
-            });
-
-        tileMesh.actionManager.registerAction(tileLeftPickAction);
-    }
-    _createTileTypeMaterial(){
-        const scene = this._scene;
-        const currTileType = this._materials.length;
-
-        const material = new BABYLON.StandardMaterial('material-tile-type-' + currTileType, scene);
-        material.ambientColor = new BABYLON.Color3(1 / (currTileType + 1), 1 / (currTileType + 1), 1 / (currTileType + 1));
-        material.freeze();
-
-        this._materials.push(material);
     }
     shouldComponentUpdate(nextProps, nextState) {
         this._oldProps = nextProps;
---
+++ js/map/MapFacade.js
@@ -0,0 +1,246 @@
+/**
+ * Created by Adrian Tello on 23.08.2016.
+ */
+
+import BABYLON from 'babylonjs';
+
+const VIEWPORT_SIZE = 5;
+
+export default class MapFacade{
+    constructor(canvas){
+        this._canvas = canvas;
+
+        this._init();
+    }
+    _init(){
+        this._initFields();
+        this._createCacheObjs();
+
+        this._initEngine();
+        this._createScene();
+        this._createCamera();
+        this._updateAspectRatio();
+    }
+    _initFields(){
+        this._size = {
+            cols: 0,
+            rows: 0
+        };
+
+        this._tileClickedListener = function(){};
+    }
+    _createCacheObjs(){
+        this._tileTypeMaterials = [];
+
+        this._firstTileMesh = null;
+        this._firstTileBorderMesh = null;
+
+        this._rows = [];
+    }
+    _initEngine(){
+        this._engine = new BABYLON.Engine(this._canvas, true);
+    }
+    _createScene(){
+        const scene =  new BABYLON.Scene(this._engine);
+        scene.shadowsEnabled = false;
+        scene.ambientColor = new BABYLON.Color3(1, 1, 1);
+        scene.lightsEnabled = false;
+
+        this._scene = scene;
+    }
+    _createCamera(){
+        const camera = new BABYLON.TargetCamera('mainCamera', new BABYLON.Vector3(0, 0, -10), this._scene);
+        camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
+
+        // attach the camera to the canvas
+        camera.attachControl(this._canvas, false);
+
+        this._mainCamera = camera;
+    }
+    _updateAspectRatio(){
+        const camera = this._mainCamera;
+
+        let height = this._canvas.height;
+        const width = this._canvas.width;
+
+        if (height == 0) height = 1;
+
+        /* Calculate aspect ratio --------------------------------------------------------------------*/
+        const aspectRatio = width / height;
+
+        if (width >= height)
+        {
+            camera.orthoLeft = -VIEWPORT_SIZE*aspectRatio;
+            camera.orthoRight = VIEWPORT_SIZE*aspectRatio;
+            camera.orthoBottom = -VIEWPORT_SIZE;
+            camera.orthoTop = VIEWPORT_SIZE;
+        }
+        else
+        {
+            camera.orthoLeft = -VIEWPORT_SIZE;
+            camera.orthoRight = VIEWPORT_SIZE;
+            camera.orthoBottom = -VIEWPORT_SIZE*aspectRatio;
+            camera.orthoTop = VIEWPORT_SIZE*aspectRatio;
+        }
+    }
+    start(){
+        const engine = this._engine;
+        const scene = this._scene;
+
+        // Register a render loop to repeatedly render the scene
+        engine.runRenderLoop(function () {
+            scene.render();
+        });
+    }
+    resize(){
+        this._updateAspectRatio();
+        this._engine.resize();
+    }
+    setTileTypes(tileTypes){
+        const oldAvailableTileTypes = this._tileTypeMaterials.length;
+
+        if(oldAvailableTileTypes > tileTypes){
+            //There are fewer tile types
+            console.warn('Not implemented yet');
+        }else if(oldAvailableTileTypes < tileTypes){
+            //There are new tile types
+            for(let tileType = oldAvailableTileTypes; tileType < tileTypes; tileType++){
+                this._createTileTypeMaterial();
+            }
+        }
+    }
+    _createTileTypeMaterial(){
+        const scene = this._scene;
+        const currTileType = this._tileTypeMaterials.length;
+
+        const material = new BABYLON.StandardMaterial('material-tile-type-' + currTileType, scene);
+        material.ambientColor = new BABYLON.Color3(1 / (currTileType + 1), 1 / (currTileType + 1), 1 / (currTileType + 1));
+        material.freeze();
+
+        this._tileTypeMaterials.push(material);
+    }
+    _setSize(numCols, numRows){
+        this._setNumCols(numCols);
+        this._setNumRows(numRows);
+    }
+    _setNumCols(numCols){
+        this._size.cols = numCols;
+
+        //TODO expand/remove existing rows
+    }
+    _setNumRows(numRows){
+        const oldRowsCount = this._rows.length;
+
+        //Check if the row number changed
+        if(oldRowsCount > numRows){
+            //There are fewer rows
+            console.warn('Not implemented yet');
+        }else if(oldRowsCount < numRows){
+            //There are new tile types
+            for(let rowNum = oldRowsCount; rowNum < numRows; rowNum++){
+                this._createTilesRow(rowNum);
+            }
+        }
+    }
+    _createTilesRow(tileY){
+        const scene = this._scene;
+
+        const newRow = [];
+
+        for (var tileX = 0; tileX < this._size.cols; tileX++){
+            const tileTag = this._createTileTag(tileX, tileY);
+
+            const tileMesh = this._createTileMesh(tileTag);
+            const tileBorderMesh = this._createTileBorderMesh(tileTag);
+            tileBorderMesh.parent = tileMesh;
+
+            const origMaterial = this._tileTypeMaterials[0];
+            tileMesh.material = origMaterial;
+            tileMesh.position.x = tileX;
+            tileMesh.position.y = tileY;
+
+            this._setUpTileActions(
+                tileMesh,
+                origMaterial,
+                {
+                    x: tileX,
+                    y: tileY
+                }
+            );
+
+            newRow.push(tileMesh);
+        }
+
+        this._rows.push(newRow);
+    }
+    _createTileTag(tileX, tileY){
+        return 'tile-' + tileX + '-' + tileY;
+    }
+    _createTileMesh(tileTag){
+        const scene = this._scene;
+        let tileMesh = null;
+
+        if(this._firstTileMesh === null){
+            this._firstTileMesh = BABYLON.Mesh.CreatePlane(tileTag, 1.0, scene);
+            tileMesh = this._firstTileMesh;
+        }else{
+            tileMesh = this._firstTileMesh.clone(tileTag);
+        }
+        return tileMesh;
+    }
+    _createTileBorderMesh(tileTag){
+        const scene = this._scene;
+
+        let tileBorderMesh = null;
+
+        const borderMeshTag = 'border-' + tileTag;
+
+        if(this._firstTileBorderMesh === null){
+            this._firstTileBorderMesh = BABYLON.Mesh.CreateLines(borderMeshTag, [
+                new BABYLON.Vector3(-0.5, -0.5, 0),
+                new BABYLON.Vector3(0.5, -0.5, 0),
+                new BABYLON.Vector3(0.5, 0.5, 0),
+                new BABYLON.Vector3(-0.5, 0.5, 0),
+                new BABYLON.Vector3(-0.5, -0.5, 0)
+            ], scene);
+
+            tileBorderMesh = this._firstTileBorderMesh;
+        }else{
+            tileBorderMesh = this._firstTileBorderMesh.clone(borderMeshTag);
+        }
+        return tileBorderMesh;
+    }
+    _setUpTileActions(tileMesh, origMaterial, tileCoords){
+        const scene = this._scene;
+
+        tileMesh.actionManager = new BABYLON.ActionManager(scene);
+
+        this._setUpTileLeftPickAction(tileMesh, tileCoords);
+    }
+    _setUpTileLeftPickAction(tileMesh, tileCoords){
+        const component = this;
+
+        const tileLeftPickAction = new BABYLON.ExecuteCodeAction(
+            BABYLON.ActionManager.OnLeftPickTrigger,
+            function () {
+                component._tileClickedListener.apply(null, [tileCoords.x, tileCoords.y]);
+            });
+
+        tileMesh.actionManager.registerAction(tileLeftPickAction);
+    }
+    setTileType(colNum, rowNum, tileType){
+        const tileMesh = this._rows[rowNum][colNum];
+
+        tileMesh.material = this._tileTypeMaterials[tileType];
+    }
+    setTileClickedListener(tileClickedListener){
+        this._tileClickedListener = tileClickedListener;
+    }
+    setDebugMode(debugMode){
+        if(debugMode){
+            this._scene.debugLayer.show();
+        }else{
+            this._scene.debugLayer.hide();
+        }
+    }
+}
