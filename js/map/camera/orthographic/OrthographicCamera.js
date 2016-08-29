546a95a7d9761b34d766bc2034553b8b8ed8fbed
Adrian Tello <adriantl7@gmail.com>
2016-08-29 21:14:09 +0200

    Create empty CameraInputsManager for the OrthographicCamera

--- js/map/camera/orthographic/OrthographicCamera.js
+++ js/map/camera/orthographic/OrthographicCamera.js
@@ -1,9 +1,26 @@
 import BABYLON from 'babylonjs';
 
+import OrthographicCameraInputsManager from './OrthographicCameraInputsManager';
+
 export default class OrthographicCamera extends BABYLON.TargetCamera{
     constructor(tag, position, scene){
         super(tag, position, scene);
 
         this.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
+
+        this._inputs = new OrthographicCameraInputsManager(this);
+    }
+    _checkInputs(){
+        this._inputs.checkInputs();
+
+        super._checkInputs();
+    }
+    attachControl(element, noPreventDefault = false){
+        this._inputs.attachElement(element, noPreventDefault);
+    }
+    detachControl(element){
+        this._inputs.detachElement(element);
+
+        this.cameraDirection = new BABYLON.Vector3(0, 0, 0);
     }
 }
---
+++ js/map/camera/orthographic/OrthographicCameraInputsManager.js
@@ -0,0 +1,5 @@
+import BABYLON from 'babylonjs';
+
+export default class OrthographicCameraInputsManager extends BABYLON.CameraInputsManager{
+    
+}
